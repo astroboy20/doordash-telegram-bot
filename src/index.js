9f0afc9f534973ee22ebfc62be08e36a4226f8c8
astroboy20 <tolulopeakinkunmi7@gmail.com>
2025-03-07 11:24:16 +0100

    feat: add Express server and webhook integration for DoorDash checker

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import dotenv from "dotenv";
 import { Telegraf, Markup } from "telegraf";
 import puppeteer from "puppeteer";
 import * as cheerio from "cheerio";
+import express from "express";
 
 dotenv.config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const PORT = process.env.PORT || 3000;
+const app = express();
 
+app.use(bot.webhookCallback("/webhook"));
+
+const WEBHOOK_URL =
+  process.env.WEBHOOK_URL || "sadly-promoted-pony.ngrok-free.app";
+
+bot.telegram.setWebhook(`${WEBHOOK_URL}/webhook`);
 bot.start((ctx) => {
   console.log("User started the bot:", ctx.from);
   ctx.reply(
@@ -26,29 +35,22 @@ bot.on("text", async (ctx) => {
     ctx.reply("Checking store status...");
 
     try {
-      // Launch a headless browser instance
       const browser = await puppeteer.launch({ headless: true });
       const page = await browser.newPage();
-
-      // Set a realistic User-Agent
       await page.setUserAgent(
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
       );
-
-      // Navigate to the URL
       await page.goto(url, { waitUntil: "networkidle0", timeout: 30000 });
-
-      // Get the page content
       const content = await page.content();
       await browser.close();
 
-      // Parse the content with cheerio
       const $ = cheerio.load(content);
       const storeName = $("h1").first().text().trim() || "Store";
       console.log("Extracted store name:", storeName);
 
-      // Check store availability
-      const storeStatus = !content.includes("This store is currently unavailable")
+      const storeStatus = !content.includes(
+        "This store is currently unavailable"
+      )
         ? "Open"
         : "Closed";
       console.log("Computed store status:", storeStatus);
@@ -74,10 +76,14 @@ bot.on("text", async (ctx) => {
       }
     } catch (error) {
       console.error("Error fetching URL with Puppeteer:", error);
-      ctx.reply("An error occurred while checking the store. Please try again later.");
+      ctx.reply(
+        "An error occurred while checking the store. Please try again later."
+      );
     }
   } else {
-    ctx.reply("Please send a valid Doordash store URL that starts with https://www.doordash.com/store/");
+    ctx.reply(
+      "Please send a valid Doordash store URL that starts with https://www.doordash.com/store/"
+    );
   }
 });
 
@@ -86,5 +92,11 @@ bot.action("check_again", (ctx) => {
   ctx.reply("Please send the Doordash store URL to check.");
 });
 
-bot.launch();
 console.log("Bot is running...");
+app.get("/", (req, res) => {
+  res.send("DoorDash Cherker is running ");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
